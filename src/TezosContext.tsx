e5d60fd2f0bbb22edeaacc6f3a296cdb466cf583
Chaim Lev-Ari <chiptus@gmail.com>
2020-12-17 19:21:40 +0200

    refactor: remove default tezos object

--- src/TezosContext.tsx
+++ src/TezosContext.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import { TezosProviderProps, State } from './types';
-import { TezosToolkit } from '@taquito/taquito';
 
-// connects to mainnet by default
-const tezos = new TezosToolkit('https://mainnet.smartpy.io');
-
-const defaultState: State = { tezos };
-
-export const TezosContext = React.createContext<State>(defaultState);
+export const TezosContext = React.createContext<State>({ tezos: null });
 
 export const useTezosContext = (): State => React.useContext(TezosContext);
 
